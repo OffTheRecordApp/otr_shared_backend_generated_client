7433422008ac77024c2746a185409e90f1a4ec31
Alex G <guirguis.a@gmail.com>
2016-04-12 17:35:22 -0700

    Adding API to remove citations from contact list

--- dist/otrBackendService.js
+++ dist/otrBackendService.js
@@ -2470,6 +2470,74 @@ angular.module('otrBackendService', [])
             /**
              * 
              * @method
+             * @name OtrService#dismissFromContactListUsingPOST
+             * @param {string} citationIdString - citationIdString
+             * 
+             */
+            OtrService.prototype.dismissFromContactListUsingPOST = function(parameters) {
+                if (parameters === undefined) {
+                    parameters = {};
+                }
+                var deferred = $q.defer();
+
+                var domain = this.domain;
+                var path = '/api/v1/citations/{citationIdString}/dismiss';
+
+                var body;
+                var queryParameters = {};
+                var headers = {};
+                var form = {};
+
+                path = path.replace('{citationIdString}', parameters['citationIdString']);
+
+                if (parameters['citationIdString'] === undefined) {
+                    deferred.reject(new Error('Missing required  parameter: citationIdString'));
+                    return deferred.promise;
+                }
+
+                if (parameters.$queryParameters) {
+                    Object.keys(parameters.$queryParameters)
+                        .forEach(function(parameterName) {
+                            var parameter = parameters.$queryParameters[parameterName];
+                            queryParameters[parameterName] = parameter;
+                        });
+                }
+
+                var url = domain + path;
+                var options = {
+                    timeout: parameters.$timeout,
+                    method: 'POST',
+                    url: url,
+                    params: queryParameters,
+                    data: body,
+                    headers: headers
+                };
+                if (Object.keys(form).length > 0) {
+                    options.data = form;
+                    options.headers['Content-Type'] = 'application/x-www-form-urlencoded';
+                    options.transformRequest = OtrService.transformRequest;
+                }
+                $http(options)
+                    .success(function(data, status, headers, config) {
+                        deferred.resolve(data);
+                        if (parameters.$cache !== undefined) {
+                            parameters.$cache.put(url, data, parameters.$cacheItemOpts ? parameters.$cacheItemOpts : {});
+                        }
+                    })
+                    .error(function(data, status, headers, config) {
+                        deferred.reject({
+                            status: status,
+                            headers: headers,
+                            config: config,
+                            body: data
+                        });
+                    });
+
+                return deferred.promise;
+            };
+            /**
+             * 
+             * @method
              * @name OtrService#getAppConfigurationUsingGET
              * 
              */
