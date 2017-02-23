8e2ce8beee0730dbb193151817182a4d2d2af640
Morkos <mark@offtherecord.com>
2017-02-22 18:15:25 -0800

    New endpoint for add secondary email address + account

--- dist/otrBackendService.js
+++ dist/otrBackendService.js
@@ -5681,6 +5681,59 @@ angular.module('otrBackendService', [])
                 return deferred.promise;
             };
             /**
+             * addExtraAccount
+             * @method
+             * @name OtrService#addExtraAccountUsingPOST
+             * @param {string} userId - userId
+             * @param {} request - request
+             * 
+             */
+            OtrService.prototype.addExtraAccountUsingPOST = function(parameters) {
+                if (parameters === undefined) {
+                    parameters = {};
+                }
+                var deferred = $q.defer();
+
+                var domain = this.domain;
+                var path = '/api/v1/users/{userId}/account';
+
+                var body;
+                var queryParameters = {};
+                var headers = {};
+                var form = {};
+
+                headers['Accept'] = ['*/*'];
+                headers['Content-Type'] = ['application/json'];
+
+                path = path.replace('{userId}', parameters['userId']);
+
+                if (parameters['userId'] === undefined) {
+                    deferred.reject(new Error('Missing required  parameter: userId'));
+                    return deferred.promise;
+                }
+
+                if (parameters['request'] !== undefined) {
+                    body = parameters['request'];
+                }
+
+                if (parameters['request'] === undefined) {
+                    deferred.reject(new Error('Missing required  parameter: request'));
+                    return deferred.promise;
+                }
+
+                if (parameters.$queryParameters) {
+                    Object.keys(parameters.$queryParameters)
+                        .forEach(function(parameterName) {
+                            var parameter = parameters.$queryParameters[parameterName];
+                            queryParameters[parameterName] = parameter;
+                        });
+                }
+
+                this.request('POST', domain + path, parameters, body, headers, queryParameters, form, deferred);
+
+                return deferred.promise;
+            };
+            /**
              * getCasesForUser
              * @method
              * @name OtrService#getCasesForUserUsingGET
