519dce86b9bec9baafe6a6661e54c01680f39df0
Morkos <mark@offtherecord.com>
2017-08-16 16:43:57 -0700

    Add target to generate node/react native JS client

--- gulpfile.js
+++ gulpfile.js
@@ -11,22 +11,53 @@ var concat  = require('gulp-concat');
 var del     = require('del');
 var sequence = require('run-sequence');
 
+var apis = [
+        {
+            swagger: 'api/otr-service.json',
+            moduleName: 'otrBackendService',
+            className: 'OtrService'
+        }
+];
+
 gulp.task('clean', function(){
     del(['dist']).then(function(paths) {
         console.log("Deleted files and folders:\n" + paths.join('\n'));
     });
 });
 
+gulp.task('otr-react-native', function(done) {
+    var codegen = require('swagger-js-react-native-codegen').CodeGen;
+    var dest = 'dist';
+    
+    apis.forEach(function(api){
+        var moduleName = api.moduleName + '-react-native';
+        
+	gutil.log('Generated ' + moduleName + '.js from ' + api.swagger);
+        
+	var swagger = JSON.parse(fs.readFileSync(api.swagger, 'utf-8'));
+        var source = codegen.getNodeCode({ moduleName: moduleName, className: api.className, swagger: swagger });
+        var generatedApiFile = dest + '/' + moduleName + '.js';
+        
+        fs.writeFileSync(generatedApiFile, source, 'UTF-8');
+
+        gulp.src([generatedApiFile])
+            //.pipe(concat('concat.js'))
+            //.pipe(rename(moduleName + '.js')) //concat file back into module.js
+            //.pipe(gulp.dest(dest))
+
+            .pipe(uglify())
+            .pipe(rename(api.moduleName + '.min.js'))
+            .pipe(gulp.dest(dest))
+
+            .pipe(notify("File has been successfully uglified!"));
+
+    });
+    done();
+});
+
 gulp.task('otr-angular', function(done){
+    
     var CodeGen = require('swagger-js-codegen').CodeGen;
-    var apis = [
-        {
-            swagger: 'api/otr-service.json',
-            moduleName: 'otrBackendService',
-            className: 'OtrService',
-            angularjs: true
-        }
-    ];
     var dest = 'dist';
     apis.forEach(function(api){
         var swagger = JSON.parse(fs.readFileSync(api.swagger, 'utf-8'));
