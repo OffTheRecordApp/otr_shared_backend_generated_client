89c668542b46107339cf6f010463354c0662c490
Morkos <mark@offtherecord.com>
2018-01-18 17:19:49 -0800

    new endpoint: publish referral contacts

--- dist/otrBackendService.js
+++ dist/otrBackendService.js
@@ -6805,6 +6805,51 @@ angular.module('otrBackendService', [])
                 return deferred.promise;
             };
             /**
+             * publishReferralContacts
+             * @method
+             * @name OtrService#publishReferralContactsUsingPOST
+             * @param {} request - request
+             * 
+             */
+            OtrService.prototype.publishReferralContactsUsingPOST = function(parameters) {
+                if (parameters === undefined) {
+                    parameters = {};
+                }
+                var deferred = $q.defer();
+
+                var domain = this.domain;
+                var path = '/api/v1/referrals/contacts';
+
+                var body;
+                var queryParameters = {};
+                var headers = {};
+                var form = {};
+
+                headers['Accept'] = ['application/json'];
+                headers['Content-Type'] = ['application/json'];
+
+                if (parameters['request'] !== undefined) {
+                    body = parameters['request'];
+                }
+
+                if (parameters['request'] === undefined) {
+                    deferred.reject(new Error('Missing required  parameter: request'));
+                    return deferred.promise;
+                }
+
+                if (parameters.$queryParameters) {
+                    Object.keys(parameters.$queryParameters)
+                        .forEach(function(parameterName) {
+                            var parameter = parameters.$queryParameters[parameterName];
+                            queryParameters[parameterName] = parameter;
+                        });
+                }
+
+                this.request('POST', domain + path, parameters, body, headers, queryParameters, form, deferred);
+
+                return deferred.promise;
+            };
+            /**
              * getUserReferralSourceTypes
              * @method
              * @name OtrService#getUserReferralSourceTypesUsingGET
