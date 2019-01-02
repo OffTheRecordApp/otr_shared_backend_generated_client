b8c9820298c5dadb0d53fbfe4382e44e1b37de03
Alex Guirguis <alex@offtherecord.com>
2019-01-02 13:26:34 -0800

    Update dispute

--- dist/otrBackendService.js
+++ dist/otrBackendService.js
@@ -5545,6 +5545,59 @@ angular.module('otrBackendService', [])
                 return deferred.promise;
             };
             /**
+             * updateDispute
+             * @method
+             * @name OtrService#updateDisputeUsingPUT
+             * @param {string} disputeId - disputeId
+             * @param {} request - request
+             * 
+             */
+            OtrService.prototype.updateDisputeUsingPUT = function(parameters) {
+                if (parameters === undefined) {
+                    parameters = {};
+                }
+                var deferred = $q.defer();
+
+                var domain = this.domain;
+                var path = '/api/v1/disputes/{disputeId}';
+
+                var body;
+                var queryParameters = {};
+                var headers = {};
+                var form = {};
+
+                headers['Accept'] = ['application/json'];
+                headers['Content-Type'] = ['application/json'];
+
+                path = path.replace('{disputeId}', parameters['disputeId']);
+
+                if (parameters['disputeId'] === undefined) {
+                    deferred.reject(new Error('Missing required  parameter: disputeId'));
+                    return deferred.promise;
+                }
+
+                if (parameters['request'] !== undefined) {
+                    body = parameters['request'];
+                }
+
+                if (parameters['request'] === undefined) {
+                    deferred.reject(new Error('Missing required  parameter: request'));
+                    return deferred.promise;
+                }
+
+                if (parameters.$queryParameters) {
+                    Object.keys(parameters.$queryParameters)
+                        .forEach(function(parameterName) {
+                            var parameter = parameters.$queryParameters[parameterName];
+                            queryParameters[parameterName] = parameter;
+                        });
+                }
+
+                this.request('PUT', domain + path, parameters, body, headers, queryParameters, form, deferred);
+
+                return deferred.promise;
+            };
+            /**
              * markDocumentWithDecision
              * @method
              * @name OtrService#markDocumentWithDecisionUsingPOST
