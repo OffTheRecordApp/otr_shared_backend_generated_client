7e16f00c5e694319647dc778af1078aca14ecab7
Alex Guirguis <alex@offtherecord.com>
2019-09-20 15:45:15 -0700

    phone leads v2

--- dist/otrBackendService.js
+++ dist/otrBackendService.js
@@ -8425,6 +8425,51 @@ angular.module('otrBackendService', [])
                 return deferred.promise;
             };
             /**
+             * addNoteToPhoneLead
+             * @method
+             * @name OtrService#addNoteToPhoneLeadUsingPOST
+             * @param {} request - request
+             * 
+             */
+            OtrService.prototype.addNoteToPhoneLeadUsingPOST = function(parameters) {
+                if (parameters === undefined) {
+                    parameters = {};
+                }
+                var deferred = $q.defer();
+
+                var domain = this.domain;
+                var path = '/api/v1/phone-leads/notes';
+
+                var body;
+                var queryParameters = {};
+                var headers = {};
+                var form = {};
+
+                headers['Accept'] = ['application/json'];
+                headers['Content-Type'] = ['application/json'];
+
+                if (parameters['request'] !== undefined) {
+                    body = parameters['request'];
+                }
+
+                if (parameters['request'] === undefined) {
+                    deferred.reject(new Error('Missing required  parameter: request'));
+                    return deferred.promise;
+                }
+
+                if (parameters.$queryParameters) {
+                    Object.keys(parameters.$queryParameters)
+                        .forEach(function(parameterName) {
+                            var parameter = parameters.$queryParameters[parameterName];
+                            queryParameters[parameterName] = parameter;
+                        });
+                }
+
+                this.request('POST', domain + path, parameters, body, headers, queryParameters, form, deferred);
+
+                return deferred.promise;
+            };
+            /**
              * getActiveRatesByArea
              * @method
              * @name OtrService#getActiveRatesByAreaUsingPOST
