83b43eb894545f555eab95779f659d590dd00ca4
Morkos <mark@offtherecord.com>
2020-05-18 20:25:39 -0700

    New endpoint: Get lawfirm fee coverage

--- dist/otrBackendService.js
+++ dist/otrBackendService.js
@@ -10555,6 +10555,41 @@ angular.module('otrBackendService', [])
                 return deferred.promise;
             };
             /**
+             * getLawfirmFeeCoverage
+             * @method
+             * @name OtrService#getLawfirmFeeCoverageUsingGET
+             * @param {object} parameters - method options and parameters
+             * @param {string} parameters.lawfirmId - lawfirmId
+             */
+            OtrService.prototype.getLawfirmFeeCoverageUsingGET = function(parameters) {
+                if (parameters === undefined) {
+                    parameters = {};
+                }
+                var deferred = $q.defer();
+                var domain = this.domain,
+                    path = '/api/v2/lawfirms/{lawfirmId}/coverage';
+                var body = {},
+                    queryParameters = {},
+                    headers = {},
+                    form = {};
+
+                headers['Accept'] = ['*/*'];
+                headers['Content-Type'] = ['application/json'];
+
+                path = path.replace('{lawfirmId}', parameters['lawfirmId']);
+
+                if (parameters['lawfirmId'] === undefined) {
+                    deferred.reject(new Error('Missing required  parameter: lawfirmId'));
+                    return deferred.promise;
+                }
+
+                queryParameters = mergeQueryParams(parameters, queryParameters);
+
+                this.request('GET', domain + path, parameters, body, headers, queryParameters, form, deferred);
+
+                return deferred.promise;
+            };
+            /**
              * deleteActionFromCase
              * @method
              * @name OtrService#deleteActionFromCaseUsingDELETE
