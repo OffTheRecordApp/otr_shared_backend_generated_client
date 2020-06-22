890ea7bbd645ef9864fa967fac8b7ffa7bafe638
Morkos <mark@offtherecord.com>
2020-06-22 15:23:01 -0700

    [TypeScript]: Use openapi-generator script to generate TS client for otr-backend-service

--- gulpfile.js
+++ gulpfile.js
@@ -7,9 +7,7 @@ var gutil   = require('gulp-util');
 var uglify  = require('gulp-uglify');
 var notify  = require('gulp-notify');
 var rename  = require('gulp-rename');
-var concat  = require('gulp-concat');
 var del     = require('del');
-var sequence = require('run-sequence');
 
 var apis = [
         {
@@ -23,36 +21,6 @@ gulp.task('clean', function(){
     del(['dist']).then(function(paths) {
         console.log("Deleted files and folders:\n" + paths.join('\n'));
     });
-});
-
-gulp.task('otr-react-native', function(done) {
-    var codegen = require('swagger-js-react-native-codegen').CodeGen;
-    var dest = 'dist';
-    
-    apis.forEach(function(api){
-        var moduleName = api.moduleName + '-react-native';
-        
-	gutil.log('Generated ' + moduleName + '.js from ' + api.swagger);
-        
-	var swagger = JSON.parse(fs.readFileSync(api.swagger, 'utf-8'));
-        var source = codegen.getNodeCode({ moduleName: moduleName, className: api.className, swagger: swagger });
-        var generatedApiFile = dest + '/' + moduleName + '.js';
-        
-        fs.writeFileSync(generatedApiFile, source, 'UTF-8');
-
-        gulp.src([generatedApiFile])
-            //.pipe(concat('concat.js'))
-            //.pipe(rename(moduleName + '.js')) //concat file back into module.js
-            //.pipe(gulp.dest(dest))
-
-            .pipe(uglify())
-            .pipe(rename(api.moduleName + '.min.js'))
-            .pipe(gulp.dest(dest))
-
-            .pipe(notify("File has been successfully uglified!"));
-
-    });
-    done();
 });
 
 gulp.task('otr-angular', function(done){
