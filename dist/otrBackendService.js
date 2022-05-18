38f7baa88a4ec7339664c2aa41e97494a66a2fab
jonathanstein@live.com <jonathanstein@live.com>
2022-05-17 17:51:00 -0700

    update

--- dist/otrBackendService.js
+++ dist/otrBackendService.js
@@ -4308,43 +4308,6 @@ angular.module('otrBackendService', [])
                 return deferred.promise;
             };
             /**
-             * getPendingAndRefusedCases
-             * @method
-             * @name OtrService#getPendingAndRefusedCasesUsingPOST
-             * @param {object} parameters - method options and parameters
-             * @param {} parameters.request - request
-             */
-            OtrService.prototype.getPendingAndRefusedCasesUsingPOST = function(parameters) {
-                if (parameters === undefined) {
-                    parameters = {};
-                }
-                var deferred = $q.defer();
-                var domain = this.domain,
-                    path = '/api/v1/console/cases/unaccepted';
-                var body = {},
-                    queryParameters = {},
-                    headers = {},
-                    form = {};
-
-                headers['Accept'] = ['application/json'];
-                headers['Content-Type'] = ['application/json'];
-
-                if (parameters['request'] !== undefined) {
-                    body = parameters['request'];
-                }
-
-                if (parameters['request'] === undefined) {
-                    deferred.reject(new Error('Missing required  parameter: request'));
-                    return deferred.promise;
-                }
-
-                queryParameters = mergeQueryParams(parameters, queryParameters);
-
-                this.request('POST', domain + path, parameters, body, headers, queryParameters, form, deferred);
-
-                return deferred.promise;
-            };
-            /**
              * getCasesWithOverduePayments
              * @method
              * @name OtrService#getCasesWithOverduePaymentsUsingPOST
