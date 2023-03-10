c6c2397bf7c86fddafee861994b73fc8a0fe0636
Morkos <morkos.cairo@gmail.com>
2023-03-10 11:51:20 -0800

    Update tsconfig for angular

---
+++ dist/angular/model/noteEntity.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.NoteEntity = void 0;
+var NoteEntity;
+(function (NoteEntity) {
+    NoteEntity.NoteTypeEnum = {
+        Case: 'CASE',
+        Lawfirm: 'LAWFIRM',
+        PhoneLead: 'PHONE_LEAD',
+        User: 'USER'
+    };
+})(NoteEntity = exports.NoteEntity || (exports.NoteEntity = {}));
---
+++ dist/angular/model/noteModel.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.NoteModel = void 0;
+var NoteModel;
+(function (NoteModel) {
+    NoteModel.NoteTypeEnum = {
+        Case: 'CASE',
+        Lawfirm: 'LAWFIRM',
+        PhoneLead: 'PHONE_LEAD',
+        User: 'USER'
+    };
+})(NoteModel = exports.NoteModel || (exports.NoteModel = {}));
