e94a6410cd79d037ec2ba05002a93d20fa73a978
jonathanstein@live.com <jonathanstein@live.com>
2023-05-02 13:19:38 -0700

    generate only ts files for angular project, remove angular2 folder

--- dist/angular/model/noteDomain.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.NoteDomain = void 0;
-var NoteDomain;
-(function (NoteDomain) {
-    NoteDomain.NoteTypeEnum = {
-        PhoneLead: 'PHONE_LEAD'
-    };
-})(NoteDomain = exports.NoteDomain || (exports.NoteDomain = {}));
