6a3e8bbc62b7b5dd5c39ac74aa95b5b0ac38c8cc
Morkos <morkos.cairo@gmail.com>
2025-04-30 14:45:17 -0700

    Experimental: Genereate TS declaration file for backend service

--- dist/typescript/api/HubspotWebhookControllerApi.d.ts
+++ dist/typescript/api/HubspotWebhookControllerApi.d.ts
@@ -9,7 +9,6 @@
  * https://openapi-generator.tech
  * Do not edit the class manually.
  */
-/// <reference types="angular" />
 export declare class HubspotWebhookControllerApi {
     protected $http: ng.IHttpService;
     protected $httpParamSerializer?: (d: any) => any;
